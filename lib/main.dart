68bfbb3fa4061d9d49ddc84bc1bbff7113916275
Mohith7548 <mohith7548@gmail.com>
2020-03-29 21:50:59 +0530

    ready to push

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Firebase Phone Auth Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Phone Authentication'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -46,68 +28,230 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  TextEditingController _phoneNumberController = TextEditingController();
+  TextEditingController _otpController = TextEditingController();
 
-  void _incrementCounter() {
+  FirebaseUser _firebaseUser;
+  String _status;
+
+  AuthCredential _phoneAuthCredential;
+  String _verificationId;
+  int _code;
+
+  @override
+  void initState() {
+    super.initState();
+    _getFirebaseUser();
+  }
+
+  Future<void> _getFirebaseUser() async {
+    this._firebaseUser = await FirebaseAuth.instance.currentUser();
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _status =
+          (_firebaseUser == null) ? 'Not Logged In\n' : 'Already LoggedIn\n';
     });
+  }
+
+  /// phoneAuthentication works this way:
+  ///     AuthCredential is the only thing that is used to authenticate the user
+  ///     OTP is only used to get AuthCrendential after which we need to authenticate with that AuthCredential
+  ///
+  /// 1. User gives the phoneNumber
+  /// 2. Firebase sends OTP if no errors occur
+  /// 3. If the phoneNumber is not in the device running the app
+  ///       We have to first ask the OTP and get `AuthCredential`(`_phoneAuthCredential`)
+  ///       Next we can use that `AuthCredential` to signIn
+  ///       This method can also be used in the previous case
+  ///    Else if user provided SIM phoneNumber is in the device running the app,
+  ///       We can signIn without the OTP.
+  ///       because the `verificationCompleted` callback gives the `AuthCredential`(`_phoneAuthCredential`) needed to signIn
+
+  Future<void> _login() async {
+    /// This method is used to login the user
+    /// `AuthCredential`(`_phoneAuthCredential`) is needed for the signIn method
+    /// After the signIn method from `AuthResult` we can get `FirebaserUser`(`_firebaseUser`)
+    try {
+      await FirebaseAuth.instance
+          .signInWithCredential(this._phoneAuthCredential)
+          .then((AuthResult authRes) {
+        _firebaseUser = authRes.user;
+        print(_firebaseUser.toString());
+      });
+      setState(() {
+        _status += 'Signed In\n';
+      });
+    } catch (e) {
+      setState(() {
+        _status += e.toString() + '\n';
+      });
+      print(e.toString());
+    }
+  }
+
+  Future<void> _logout() async {
+    /// Method to Logout the `FirebaseUser` (`_firebaseUser`)
+    try {
+      // signout code
+      await FirebaseAuth.instance.signOut();
+      setState(() {
+        _status += 'Signed out\n';
+      });
+    } catch (e) {
+      setState(() {
+        _status += e.toString() + '\n';
+      });
+      print(e.toString());
+    }
+  }
+
+  Future<void> _submitPhoneNumber() async {
+    /// NOTE: Either append your phone number country code or add in the code itself
+    /// Since I'm in India we use "+91 " as prefix `phoneNumber`
+    String phoneNumber = "+91 " + _phoneNumberController.text.toString().trim();
+    print(phoneNumber);
+
+    /// The below functions are the callbacks, separated so as to make code more redable
+    void verificationCompleted(AuthCredential phoneAuthCredential) {
+      print('verificationCompleted');
+      setState(() {
+        _status += 'verificationCompleted\n';
+      });
+      this._phoneAuthCredential = phoneAuthCredential;
+      print(phoneAuthCredential);
+    }
+
+    void verificationFailed(AuthException error) {
+      print('verificationFailed');
+      setState(() {
+        _status += '$error\n';
+      });
+      print(error);
+    }
+
+    void codeSent(String verificationId, [int code]) {
+      print('codeSent');
+      this._verificationId = verificationId;
+      print(verificationId);
+      this._code = code;
+      print(code.toString());
+      setState(() {
+        _status += 'Code Sent\n';
+      });
+    }
+
+    void codeAutoRetrievalTimeout(String verificationId) {
+      print('codeAutoRetrievalTimeout');
+      setState(() {
+        _status += 'codeAutoRetrievalTimeout\n';
+      });
+      print(verificationId);
+    }
+
+    await FirebaseAuth.instance.verifyPhoneNumber(
+      /// Make sure to prefix with your country code
+      phoneNumber: phoneNumber,
+
+      /// `seconds` didn't work. The underlying implementation code only reads in `millisenconds`
+      timeout: Duration(milliseconds: 10000),
+
+      /// If the SIM (with phoneNumber) is in the current device this function is called.
+      /// This function gives `AuthCredential`. Moreover `login` function can be called from this callback
+      verificationCompleted: verificationCompleted,
+
+      verificationFailed: verificationFailed,
+
+      /// This is called after the OTP is sent. Gives a `verificationId` and `code`
+      codeSent: codeSent,
+
+      codeAutoRetrievalTimeout: codeAutoRetrievalTimeout,
+    ); // All the callbacks are above
+  }
+
+  void _submitOTP() {
+    String smsCode = _otpController.text.toString().trim();
+
+    /// when used different phoneNumber other than the current (running) device
+    /// we need to use OTP to get `phoneAuthCredential` which is inturn used to signIn/login
+    this._phoneAuthCredential = PhoneAuthProvider.getCredential(
+        verificationId: this._verificationId, smsCode: smsCode);
+
+    _login();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: ListView(
+        padding: EdgeInsets.all(16),
+        // mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          SizedBox(height: 24),
+          Row(
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: TextField(
+                  controller: _phoneNumberController,
+                  decoration: InputDecoration(
+                    hintText: 'Phone Number',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              Spacer(),
+              Expanded(
+                flex: 1,
+                child: MaterialButton(
+                  onPressed: _submitPhoneNumber,
+                  child: Text('Submit'),
+                  color: Theme.of(context).accentColor,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 48),
+          Row(
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: TextField(
+                  controller: _otpController,
+                  decoration: InputDecoration(
+                    hintText: 'OTP',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              Spacer(),
+              Expanded(
+                flex: 1,
+                child: MaterialButton(
+                  onPressed: _submitOTP,
+                  child: Text('Submit'),
+                  color: Theme.of(context).accentColor,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 48),
+          Text('$_status'),
+          SizedBox(height: 48),
+          MaterialButton(
+            onPressed: _login,
+            child: Text('Login'),
+            color: Theme.of(context).accentColor,
+          ),
+          SizedBox(height: 24),
+          MaterialButton(
+            onPressed: _logout,
+            child: Text('Logout'),
+            color: Theme.of(context).accentColor,
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
